66818898816399e80ded5f7eab5366da1cf95ec2
UndersunIO <alex.atc.digital@gmail.com>
2021-10-10 19:20:44 +0200

    rendu conditionnel avant chatbox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Membre from './components/Membre';
-import Presentation from './components/Presentation';
+//import Presentation from './components/Presentation';
 import Button from './components/button';
 
 const famille = {
@@ -21,55 +21,69 @@ const famille = {
 
 class App extends Component {
   state = {
-    famille
+    famille,
+    isShow: false
   }
-// handle pour les evenement. quand on clique sur le button alors console log est declenché
-// ...this.state.famille recupere l'état, ajoute un an au click, passe par la fonction setState pour modifier l'état 
-  handleClick = (num) => {
-    const famille = {...this.state.famille};
-    famille.membre1.age += num;
-    famille.membre2.age += 1;
-    famille.membre3.age += 2;
-    this.setState({famille});
-  }
+    // handle pour les evenement. quand on clique sur le button alors console log est declenché
+    // ...this.state.famille recupere l'état, ajoute un an au click, passe par la fonction setState pour modifier l'état 
+    handleClick = num => {
+      const famille = {...this.state.famille};
+      famille.membre1.age += num;
+      this.setState({famille});
+    }
 
-  handleChange = (event) => {
-    const famille = {...this.state.famille};
-    const nom = event.target.value;
-    const age = event.target.value;
-    console.log(nom);
-    famille.membre1.nom = nom;
-    famille.membre1.age = age;
-    this.setState({famille});
-  }
+    handleChange = event => {
+      const famille = {...this.state.famille};
+      const nom = event.target.value;
+      famille.membre1.nom = nom;
+      this.setState({famille});
+    }
 
-  render() {
-    const {famille} = this.state;
-    return (
-      <div className="App">
-        <h1>React</h1>
-        <input value={famille.membre1.nom} onChange={this.handleChange} type='text '></input>
-        <input value={famille.membre1.age} onChange={this.handleChange} type='text '></input>
+    handleShowDescription = () => {
+      // ! permet de renvoyer toujours l'inverse
+      const isShow = !this.state.isShow
+      this.setState({ isShow })
+
+    }
+    render() {
+      const { titre } = this.props
+      const {famille, isShow } = this.state
+
+      let description = null
+
+      if (isShow) {
+        description = <strong>Je suis un props children en gras </strong>
+      }
+
+      const liste = Object.keys(famille)
+      .map(membre => (
         <Membre 
-          nom={famille.membre1.nom}
-          age={famille.membre1.age} />
-        <Membre 
-          nom={famille.membre2.nom}
-          age={famille.membre2.age} />
-        <Membre 
-          nom={famille.membre3.nom}
-          age={famille.membre3.age} />
-        
-        <Membre nom="Props">
-          <strong>Je suis un props children en gras</strong>
-        </Membre>
-        <Presentation />
-       <Button 
-          vieillir={() => this.handleClick(5)}
-       />
-      </div>
-    );
+            nom={famille.[membre].nom}
+            age={famille.[membre].age} />
+
+      ))
+      console.log (liste)
+      return (
+        <div className="App">
+           <h1>{titre}</h1>
+          <input value={famille.membre1.nom} onChange={this.handleChange} type='text '></input>
+            {liste} 
+          <Membre nom="Props">
+            { description }
+            <button onClick={this.handleShowDescription}>
+            {
+              isShow ? 'Cacher' : 'Montrer'
+            } 
+            </button>
+          
+          </Membre>
+         {/* <Presentation /> */} 
+        <Button 
+            vieillir={() => this.handleClick(1)}
+        />
+        </div>
+      );
+    }
   }
-}
 
 export default App;
--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = ({vieillir}) => (
     <button onClick={vieillir}>
-        vieillir De 5,1,2 ans
+        vieillir d'1 ans Trinity
     </button>
     )
 
